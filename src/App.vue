ec814efe207e3126b2001065d18a581d645bf429
zachary <zlj19971222@outlook.com>
2022-04-19 11:24:27 +0800

    init project.

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Input from "./components/Input.vue";
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Input />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  margin-top: 110px;
+  text-align: center;
+  background: #d8d8d6;
+}
+</style>
