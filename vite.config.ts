85c4e06d9e5e42d440cf59edfb27532772604b4f
zachary <zlj19971222@outlook.com>
2022-04-21 00:34:30 +0800

    (chore): update project config
    - add .env supported.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log(process.cwd())
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue({
-		reactivityTransform: true
-	})]
-})
+export default ({ mode }) => {
+	return defineConfig({
+		plugins: [vue({
+			reactivityTransform: true
+		})],
+		base: loadEnv(mode, process.cwd()).VITE_APP_NAME
+	})
+}
