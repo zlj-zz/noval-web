f1d1b46511f713a8b14f724e5432841ea028870b
zachary <zlj19971222@outlook.com>
2022-04-22 17:35:02 +0800

    (feat): create mimicry-list.

---
+++ src/components/MimicryList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+
+const props = defineProps({
+  width: { type: String, default: "300px" },
+});
+</script>
+
+<template>
+  <ul class="mimicry-ul">
+    <slot></slot>
+  </ul>
+</template>
+
+<style>
+.mimicry-ul {
+  width: v-bind(width);
+
+  margin: auto;
+  position: relative;
+}
+
+.mimicry-ul li {
+  list-style-type: none;
+
+  margin-top: 1.2rem;
+  height: 2rem;
+
+  text-align: left;
+  color: rgb(57, 61, 61);
+
+  border-radius: 5px;
+  background: #d8d8d6;
+  box-shadow: 20px 20px 60px #b6b7b5, -20px -20px 60px #f9faf7;
+  border: 0px solid silver;
+}
+
+.mimicry-ul li:hover {
+  background: #d0d0ce;
+}
+</style>
