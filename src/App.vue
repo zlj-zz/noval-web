ef0820d73979f03783a5aded899664b4c2b46ba0
zachary <zlj19971222@outlook.com>
2022-05-05 23:21:48 +0800

    (feat): Adaptive footer height.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,38 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
+import { onMounted, ref, watch } from 'vue';
+
+const footTop = ref<string>('30px')
+const scrollHeight = ref(0)
+
+function adjustFootTop(scrollHeight: number) {
+  let innerHeight = window.innerHeight
+  if (innerHeight - scrollHeight > 100) {
+    let excessHeight = innerHeight - scrollHeight - 15
+    footTop.value = `${excessHeight}px`
+  } else if (innerHeight - scrollHeight < -20) {
+    footTop.value = `30px`
+  }
+  console.log('scrollHeight:', scrollHeight, 'innerHeight:', innerHeight)
+}
+
+let timer = null
+onMounted(() => {
+  scrollHeight.value = document.body.scrollHeight
+  adjustFootTop(scrollHeight.value)
+
+  timer = setInterval(() => {
+    scrollHeight.value = document.body.scrollHeight
+  }, 200)
+
+  watch(
+    scrollHeight,
+    (scrollHeight, oldHeight) => adjustFootTop(scrollHeight)
+  )
+})
+
+
 </script>
 
 <template>
@@ -10,9 +42,11 @@
     <span class="title-name">Noval</span>
   </div>
   <div class="content">
-    <router-view v-slot="{ Component }"></router-view>
+    <router-view v-slot="{ Component }">
+      <component :is="Component" view-prop="value"></component>
+    </router-view>
   </div>
-  <div class="footer">
+  <div class="footer" :style="{ top: footTop }">
     <a href="https://github.com/zlj-zz/noval-web" target="_blank">https://github.com/zlj-zz/noval-web</a>
   </div>
 </template>
