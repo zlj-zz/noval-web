5580195481a725278710fd960162cde3a0ab23bd
zachary <zlj19971222@outlook.com>
2022-05-06 00:46:06 +0800

    (fix): process special code in url.

--- src/api/out.ts
+++ src/api/out.ts
@@ -4,10 +4,10 @@ import axiosInstance from "./request";
 export const searchFictions = (name: string) => axiosInstance.get(`/fiction?name=${name}`)
 
 // Crawl fiction by key.
-export const crawlFiction = (key: string) => axiosInstance.get(`/crawl?key=${key}`)
+export const crawlFiction = (key: string) => axiosInstance.get(`/crawl?key=${encodeURIComponent(key)}`)
 
 // Get the status of crawling fiction by key.
-export const getCrawlStatus = (key: string) => axiosInstance.get(`crawl_status?key=${key}`)
+export const getCrawlStatus = (key: string) => axiosInstance.get(`crawl_status?key=${encodeURIComponent(key)}`)
 
 // Download fiction to local.
 // export const downloadFiction = (key: string) => axiosInstance.get(`/download?key=${key}`)
